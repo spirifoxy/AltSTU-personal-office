304e8915389bbed239e9ea0cf8f98b16fba6a36b
TolichP <sanekprivor@yandex.ru>
2017-05-10 22:06:36 +0700

    News alpha-version

---
+++ app/src/main/java/com/tolichp/spirifoxy/altstu_personal_office/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.tolichp.spirifoxy.altstu_personal_office;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tolichp.spirifoxy.altstu_personal_office.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
