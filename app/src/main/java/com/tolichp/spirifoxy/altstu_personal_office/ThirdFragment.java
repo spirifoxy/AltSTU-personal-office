4fa9b85dfc34c77b780cf906d2d6db385b1aaf3e
TolichP <sanekprivor@yandex.ru>
2017-05-15 23:22:18 +0700

    Peace of menu

---
+++ app/src/main/java/com/tolichp/spirifoxy/altstu_personal_office/ThirdFragment.java
@@ -0,0 +1,19 @@
+package com.tolichp.spirifoxy.altstu_personal_office;
+
+        import android.app.Fragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+public class ThirdFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_third, container, false);
+
+        return rootView;
+    }
+}
