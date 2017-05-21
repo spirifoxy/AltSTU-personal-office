739e71ce0d4c944fbff7cd80c650c73ccbb071e5
TolichP <sanekprivor@yandex.ru>
2017-05-21 20:36:56 +0700

    Messages for teacher

---
+++ app/src/teacher/java/com/tolichp/spirifoxy/altstu_personal_office/student/TeacherMessages.java
@@ -0,0 +1,13 @@
+package com.tolichp.spirifoxy.altstu_personal_office.student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherMessages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_messages);
+    }
+}
