7cad9e3f6f45f8199a9ea8325e3b75ab90ff5f49
TolichP <sanekprivor@yandex.ru>
2017-05-21 23:36:29 +0700

    Bug fix

--- app/src/student/java/com/tolichp/spirifoxy/altstu_personal_office/StudentNavigationMenuActivity.java
+++ app/src/student/java/com/tolichp/spirifoxy/altstu_personal_office/StudentNavigationMenuActivity.java
@@ -1,20 +1,11 @@
 package com.tolichp.spirifoxy.altstu_personal_office;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 public class StudentNavigationMenuActivity extends NavigationMenuActivity {
 
@@ -40,7 +31,7 @@ public class StudentNavigationMenuActivity extends NavigationMenuActivity {
         } else if (id == R.id.nav_slideshow) {
             fragment = StudyProgressFragment.newInstance();
         } else if (id == R.id.nav_manage) {
-            fragment = SessionsSemFragment.newInstance();
+            fragment = SessionsFragment.newInstance();
 //        } else if (id == R.id.nav_share) {
 //
 //        } else if (id == R.id.nav_send) {
