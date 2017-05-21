6ad1b96915f7f8a2787d75b38f886780177c1e87
spirifoxy <spirit.ixi@gmail.com>
2017-05-21 23:44:45 +0700

    Structure changes

--- app/src/teacher/java/com/tolichp/spirifoxy/altstu_personal_office/TeacherMessages.java
+++ app/src/teacher/java/com/tolichp/spirifoxy/altstu_personal_office/TeacherMessages.java
@@ -1,4 +1,4 @@
-package com.tolichp.spirifoxy.altstu_personal_office.student;
+package com.tolichp.spirifoxy.altstu_personal_office;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
