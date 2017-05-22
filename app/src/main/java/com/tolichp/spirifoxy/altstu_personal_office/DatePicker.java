2009b3c7429394d9fe1b405529ae5e7144f9dd2a
TolichP <sanekprivor@yandex.ru>
2017-05-23 01:09:34 +0700

    DatePicker for timetable, spinner for choosing week

---
+++ app/src/main/java/com/tolichp/spirifoxy/altstu_personal_office/DatePicker.java
@@ -0,0 +1,47 @@
+package com.tolichp.spirifoxy.altstu_personal_office;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.Button;
+
+import java.util.Calendar;
+
+public class DatePicker extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // определяем текущую дату
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // создаем DatePickerDialog и возвращаем его
+        Dialog picker = new DatePickerDialog(getActivity(), this,
+                year, month, day);
+        picker.setTitle(getResources().getString(R.string.choose_date));
+
+        return picker;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // добавляем кастомный текст для кнопки
+        Button nButton =  ((AlertDialog) getDialog())
+                .getButton(DialogInterface.BUTTON_POSITIVE);
+        nButton.setText(getResources().getString(R.string.ready));
+
+    }
+
+    @Override
+    public void onDateSet(android.widget.DatePicker datePicker, int year,
+                          int month, int day) {
+
+    }
+}
